5ea265a4b89a62ac42a11f7225ce9b7fe8841732
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-27 02:59:28 +0530

    Update cust_filters_sql.py

--- Naomi/modules/sql/cust_filters_sql.py
+++ Naomi/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, BigInteger, distinct, func
 
-from FallenRobot.modules.helper_funcs.msg_types import Types
-from FallenRobot.modules.sql import BASE, SESSION
+from Naomi.modules.helper_funcs.msg_types import Types
+from Naomi.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
