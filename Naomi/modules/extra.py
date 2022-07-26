910ae29f85bac18e0f91acfb8a146c1abbdc2632
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-27 02:02:46 +0530

    Add files via upload

---
+++ Naomi/modules/extra.py
@@ -0,0 +1,17 @@
+__mod_name__ = "Tools"
+__help__ ="""
+*Available queries:* Country Code/Country Name/Timezone Name
+  ❍ /time <query>*:* Gives information about a timezone.
+ ❍ ⏰ [Timezones list](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+ ❍ /country <country name>*:* Gathering info about given country
+ ❍ /weather <city>*:* Get weather info in a particular place.
+ 
+*Some cmds for fun*
+
+ ❍ /wttr <city>*:* Advanced weather module, usage same as /weather
+ ❍ /wttr moon*:* Get the current status of moon
+ ❍ /tiny*:* reply a sticker and see magic
+ ❍ /github <username>*:* Get information about a GitHub user.
+ ❍ /webss *:* Sends the screenshot of the given url.
+ """
+ 
---
+++ Naomi/modules/musicals.py
@@ -0,0 +1,7 @@
+__mod_name__ = "Mᴜsɪᴄ"
+
+__help__ = """
+❍ /vsong <link of youtube video or name of song>*:* Uploads the video in it's best quality available.
+❍ /song <song name>*:* Uploads the song in it's best quality available
+ 💡Ex: `/vsong or /song Faded Alan Walker`
+"""
