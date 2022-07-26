ca9a30ef32eb962f3f2d454528fe1751033604bd
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-27 02:54:56 +0530

    Update __init__.py

--- Naomi/modules/sql/__init__.py
+++ Naomi/modules/sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from FallenRobot import DB_URI, LOGGER as log
+from Naomi import DB_URI, LOGGER as log
 
 if DB_URI and DB_URI.startswith("postgres://"):
     DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
