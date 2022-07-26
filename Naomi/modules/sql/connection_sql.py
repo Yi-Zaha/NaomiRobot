69c17eceee302b44b38a5f0f9ca8e9ad49e86731
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-27 02:58:52 +0530

    Update connection_sql.py

--- Naomi/modules/sql/connection_sql.py
+++ Naomi/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import Column, String, Boolean, UnicodeText, BigInteger
 
-from FallenRobot.modules.sql import SESSION, BASE
+from Naomi.modules.sql import BASE, SESSION
 
 
 class ChatAccessConnectionSettings(BASE):
