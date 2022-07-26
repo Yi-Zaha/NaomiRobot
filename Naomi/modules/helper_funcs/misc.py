b5d60e98f97eddfa89609284429c9370c64b4b40
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-27 02:52:17 +0530

    Update misc.py

--- Naomi/modules/helper_funcs/misc.py
+++ Naomi/modules/helper_funcs/misc.py
@@ -13,7 +13,7 @@ from telegram import (
 )
 from telegram.error import TelegramError
 
-from FallenRobot import NO_LOAD
+from Naomi import NO_LOAD
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
