9e7a1a72ba5562fb1c76fa149b264e93d6d9b50e
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-27 09:13:46 +0530

    Update admin.py

--- Naomi/modules/admin.py
+++ Naomi/modules/admin.py
@@ -1037,7 +1037,7 @@ dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
 
-__mod_name__ = "Aᴅᴍɪɴs"
+__mod_name__ = "🕶️ Aᴅᴍɪɴs 🕶️"
 __command_list__ = [
     "setdesc" "setsticker" "setgpic" "delgpic" "setgtitle" "adminlist",
     "admins",
