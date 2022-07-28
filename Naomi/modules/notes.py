5c6ebfe37db77949776e8f7bfdc36d737d526731
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-28 20:50:23 +0530

    Update notes.py

--- Naomi/modules/notes.py
+++ Naomi/modules/notes.py
@@ -3,14 +3,14 @@ from io import BytesIO
 import random
 from typing import Optional
 
-import FallenRobot.modules.sql.notes_sql as sql
-from FallenRobot import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.handlers import MessageHandlerChecker
-from FallenRobot.modules.helper_funcs.chat_status import user_admin, connection_status
-from FallenRobot.modules.helper_funcs.misc import build_keyboard, revert_buttons
-from FallenRobot.modules.helper_funcs.msg_types import get_note_type
-from FallenRobot.modules.helper_funcs.string_handling import (
+import Naomi.modules.sql.notes_sql as sql
+from Naomi import LOGGER, JOIN_LOGGER, SUPPORT_CHAT, dispatcher, DRAGONS
+from Naomi.modules.disable import DisableAbleCommandHandler
+from Naomi.modules.helper_funcs.handlers import MessageHandlerChecker
+from Naomi.modules.helper_funcs.chat_status import user_admin, connection_status
+from Naomi.modules.helper_funcs.misc import build_keyboard, revert_buttons
+from Naomi.modules.helper_funcs.msg_types import get_note_type
+from Naomi.modules.helper_funcs.string_handling import (
     escape_invalid_curly_brackets,
 )
 from telegram import (
@@ -542,7 +542,7 @@ A button can be added to a note by using standard markdown link syntax - the lin
 
 """
 
-__mod_name__ = "Nᴏᴛᴇs"
+__mod_name__ = "📔🖌 Notes 📔🖌"
 
 GET_HANDLER = CommandHandler("get", cmd_get)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get)
