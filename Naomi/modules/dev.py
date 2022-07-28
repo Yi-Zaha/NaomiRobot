ad79ce09410020da38548df9f27163f520d0b7fc
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-28 17:49:28 +0530

    Update dev.py

--- Naomi/modules/dev.py
+++ Naomi/modules/dev.py
@@ -5,10 +5,10 @@ import sys
 from contextlib import suppress
 from time import sleep
 
-import FallenRobot
+import Naomi
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+from Naomi import dispatcher
+from Naomi.modules.helper_funcs.chat_status import dev_plus
 from telegram import TelegramError, Update
 from telegram.error import Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, run_async
