478d86603435c208aee616f87373623dc19f84fd
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-28 08:26:44 +0530

    Update admin.py

--- Naomi/modules/admin.py
+++ Naomi/modules/admin.py
@@ -6,9 +6,9 @@ from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from FallenRobot import DRAGONS, dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
-from FallenRobot.modules.helper_funcs.chat_status import (
+from Naomi import DRAGONS, dispatcher
+from Naomi.modules.disable import DisableAbleCommandHandler
+from Naomi.modules.helper_funcs.chat_status import (
     bot_admin,
     can_pin,
     can_promote,
@@ -17,17 +17,17 @@ from FallenRobot.modules.helper_funcs.chat_status import (
     ADMIN_CACHE,
 )
 
-from FallenRobot.modules.helper_funcs.admin_rights import (
+from Naomi.modules.helper_funcs.admin_rights import (
     user_can_changeinfo,
     user_can_promote,
 )
-from FallenRobot.modules.helper_funcs.extraction import (
+from Naomi.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from FallenRobot import SUPPORT_CHAT
-from FallenRobot.modules.log_channel import loggable
-from FallenRobot.modules.helper_funcs.alternate import send_message
+from Naomi import SUPPORT_CHAT
+from Naomi.modules.log_channel import loggable
+from Naomi.modules.helper_funcs.alternate import send_message
 
 
 @run_async
