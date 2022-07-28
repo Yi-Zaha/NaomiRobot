a2e974f3023665a4694c10daa050d2759274e72e
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-28 17:14:38 +0530

    Update currency_converter.py

--- Naomi/modules/currency_converter.py
+++ Naomi/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from FallenRobot import CASH_API_KEY, dispatcher
+from Naomi import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
