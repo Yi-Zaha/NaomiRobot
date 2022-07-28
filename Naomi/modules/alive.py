34564712cd4d667e1ea5658b812e72a23e0bdb6b
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-28 08:34:53 +0530

    Update alive.py

--- Naomi/modules/alive.py
+++ Naomi/modules/alive.py
@@ -7,8 +7,8 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from FallenRobot.events import register
-from FallenRobot import telethn as tbot, SUPPORT_CHAT, OWNER_USERNAME, dispatcher,pbot as app
+from Naomi.events import register
+from Naomi import telethn as tbot, SUPPORT_CHAT, OWNER_USERNAME, dispatcher,pbot as app
 
 
 PHOTO = [
@@ -16,7 +16,7 @@ PHOTO = [
     "https://telegra.ph/file/4f66f78c620693de0bb42.jpg",
 ]
 
-@app.on_message(filters.command("start") & filters.group)
+@app.on_message(filters.command("start") & ~filters.group)
 async def awake(event):
     TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
     TEXT += f"» **Owner :** [𝙺𝙰𝙶𝚄𝚃𝚂𝚄𝙲𝙷𝙸](https://t.me/{OWNER_USERNAME})** \n\n"
