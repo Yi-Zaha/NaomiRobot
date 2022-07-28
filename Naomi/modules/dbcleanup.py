2ed344374815c71e25c4fa6a4a562fb76cd129d5
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-28 17:19:16 +0530

    Update dbcleanup.py

--- Naomi/modules/dbcleanup.py
+++ Naomi/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import FallenRobot.modules.sql.global_bans_sql as gban_sql
-import FallenRobot.modules.sql.users_sql as user_sql
-from FallenRobot import DEV_USERS, OWNER_ID, dispatcher
-from FallenRobot.modules.helper_funcs.chat_status import dev_plus
+import Naomi.modules.sql.global_bans_sql as gban_sql
+import Naomi.modules.sql.users_sql as user_sql
+from Naomi import DEV_USERS, OWNER_ID, dispatcher
+from Naomi.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
