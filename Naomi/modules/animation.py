4e703f16fefe47007d9d56f1a51a9af3036f03b4
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-29 20:05:25 +0530

    Update animation.py

--- Naomi/modules/animation.py
+++ Naomi/modules/animation.py
@@ -293,7 +293,7 @@ dispatcher.add_handler(MOONANIMATION_HANDLER)
 dispatcher.add_handler(CLOCKANIMATION_HANDLER)
 dispatcher.add_handler(BLOCKANIMATION_HANDLER)
 
-__mod_name__ = "Animation"
+__mod_name__ = "🏮 Animation 🏮"
 __command_list__ = [
     "love",
     "hack",
@@ -313,3 +313,16 @@ __handlers__ = [
     EARTHANIMATION_HANDLER,
     BLOCKANIMATION_HANDLER,
     KILL_HANDLER,
+]
+__help__="""
+Here is the help for the Animation module:
+
+- /love ❣️
+- /hack 👨‍💻
+- /bombs 💣
+- /moon 🌖
+- /clock 🕑 
+- /earth 🌏
+- /kill 💀
+- /block🚫
+"""
