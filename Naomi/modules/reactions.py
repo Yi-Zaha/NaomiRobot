0250570eae694cd52828f7277d9587124f9ed0f7
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-29 19:01:07 +0530

    Update reactions.py

--- Naomi/modules/reactions.py
+++ Naomi/modules/reactions.py
@@ -1,7 +1,7 @@
 import random
 
-from FallenRobot import dispatcher
-from FallenRobot.modules.disable import DisableAbleCommandHandler
+from Naomi  import dispatcher
+from Naomi .modules.disable import DisableAbleCommandHandler
 from telegram import Update
 from telegram.ext import CallbackContext, run_async
 
