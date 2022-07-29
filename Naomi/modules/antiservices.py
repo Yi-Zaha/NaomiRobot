f147e52c5d571881d03eaa87af9ba7fcd4e91dbc
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-29 20:18:36 +0530

    Create antiservices.py

---
+++ Naomi/modules/antiservices.py
@@ -0,0 +1,19 @@
+__help__="""
+*Help for module Antiflood:* 
+ ❍ /flood*:* Get the current flood control setting
+• *Admins only:*
+ ❍ /setflood <int/'no'/'off'>*:* enables or disables flood control
+ *Example:* `/setflood 10`
+ ❍ /setfloodmode <ban/kick/mute/tban/tmute> <value>*:* Action to perform when user have exceeded flood limit. ban/kick/mute/tmute/tban
+• *Note:*
+ • Value must be filled for tban and tmute!!
+ It can be:
+ `5m` = 5 minutes
+ `6h` = 6 hours
+ `3d` = 3 days
+ `1w` = 1 week
+
+*Help for module Anti-NSFW:*
+❍ /antinsfw on/enable*:*Turn on to protect your chat from  NSFW content.
+❍ /antinsfw off/disable*:* Allow NSFW content in group/chat
+❍ /nsfwscan*:*Scan adult content in chat
