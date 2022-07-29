5b5eeb4c067daf2aaee62cb003b3d8444e1087c5
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-29 14:25:46 +0530

    Update tiny.py

--- Naomi/modules/tiny.py
+++ Naomi/modules/tiny.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 from PIL import Image
-from FallenRobot.events import register
-from FallenRobot import telethn as tbot
+from Naomi.events import register
+from Naomi import telethn as tbot
 
 
 @register(pattern="^/tiny ?(.*)")
