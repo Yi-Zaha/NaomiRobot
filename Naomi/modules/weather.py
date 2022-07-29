c822885d98dc5a92c6902e1176f54dfb9fa6d16e
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-29 13:47:47 +0530

    Update weather.py

--- Naomi/modules/weather.py
+++ Naomi/modules/weather.py
@@ -1,4 +1,4 @@
-from FallenRobot import telethn as tbot
+from Naomi import telethn as tbot
 import io
 import os
 import time
@@ -9,9 +9,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from FallenRobot import *
+from Naomi import *
 
-from FallenRobot.events import register
+from Naomi.events import register
 
 
 async def is_register_admin(chat, user):
