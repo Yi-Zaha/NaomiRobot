90958d3fc166b5ae5b1da2535d6d9ae20972260f
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-07-30 22:00:03 +0530

    Update karma.py

--- Naomi/modules/karma.py
+++ Naomi/modules/karma.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from aiohttp import ClientSession
 from Python_ARQ import ARQ
 
-from Naomi import pbot as app, BOT_ID
+from Naomi import pbot as app
 from Naomi.utils.errors import capture_err
 from Naomi.utils.permissions import adminsOnly
 from Naomi.function.dbfunctions import (
