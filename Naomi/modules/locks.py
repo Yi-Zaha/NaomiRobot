6e2f30bc90bc78efb38c61df4cd9626e975a0012
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-08-01 17:47:24 +0530

    Update locks.py

--- Naomi/modules/locks.py
+++ Naomi/modules/locks.py
@@ -585,8 +585,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  • Unlocking permission *pin* will allow members (non-admins) to pin a message in a group
 """
 
-__mod_name__ = "ʟᴏᴄᴋꜱ"
-
+__mod_name__ ="ʟᴏᴄᴋꜱ"
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
 UNLOCK_HANDLER = CommandHandler(
