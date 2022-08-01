c800297b71493e78d4c956158ddf5373c18c44c3
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-08-01 17:06:33 +0530

    Update stickers.py

--- Naomi/modules/stickers.py
+++ Naomi/modules/stickers.py
@@ -456,7 +456,7 @@ __help__ = """
  ❍ /stickers*:* Find stickers for given term on combot sticker catalogue
 """
 
-__mod_name__ = "🌫️ Stickers 🌫️"
+__mod_name__ = "ꜱᴛɪᴄᴋᴇʀꜱ"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
 GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
 KANG_HANDLER = DisableAbleCommandHandler("kang", kang, admin_ok=True)
