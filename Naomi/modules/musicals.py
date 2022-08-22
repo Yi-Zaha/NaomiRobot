2c37965802283117768520a21fb8988050141943
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-08-22 20:57:01 +0530

    Update musicals.py

--- Naomi/modules/musicals.py
+++ Naomi/modules/musicals.py
@@ -1,4 +1,4 @@
-__mod_name__ ="ꜱᴏɴɢ"
+__mod_name__ ="🇸ᴏɴɢ"
 __help__ = """
 ❍ /vsong <link of youtube video or name of song>*:* Uploads the video in it's best quality available.
 ❍ /song <song name>*:* Uploads the song in it's best quality available
