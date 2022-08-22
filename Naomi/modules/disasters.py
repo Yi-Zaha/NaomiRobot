acf3c70d99314d7c4b740337bbbbe02ba0e91029
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-08-22 20:44:08 +0530

    Update disasters.py

--- Naomi/modules/disasters.py
+++ Naomi/modules/disasters.py
@@ -730,7 +730,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "ᴅɪꜱᴀꜱᴛᴇʀꜱ"
+__mod_name__ = "🇩ɪꜱᴀꜱᴛᴇʀꜱ"
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
