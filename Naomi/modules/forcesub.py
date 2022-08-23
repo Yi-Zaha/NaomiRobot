caad70f4119112dd39140346348897477b949b17
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-08-23 18:47:29 +0530

    Update forcesub.py

--- Naomi/modules/forcesub.py
+++ Naomi/modules/forcesub.py
@@ -12,7 +12,7 @@ from pyrogram.types import ChatPermissions, InlineKeyboardButton, InlineKeyboard
 
 from Naomi import DRAGONS as SUDO_USERS
 from Naomi import pbot
-from Naomi.mongo1 import fsubdb as sql
+from Naomi.modules.sql import forceSubscribe_sql as sql
 
 
 logging.basicConfig(level=logging.INFO)
