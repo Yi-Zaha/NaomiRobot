af72f99d51cb878b0660687429fa0168ec00666f
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-08-24 20:38:54 +0530

    Update rules.py

--- Naomi/modules/rules.py
+++ Naomi/modules/rules.py
@@ -17,7 +17,6 @@ from Naomi import dispatcher
 from Naomi.modules.helper_funcs.chat_status import user_admin
 from Naomi.modules.helper_funcs.string_handling import markdown_parser
 
-
 @run_async
 def get_rules(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
@@ -35,8 +34,7 @@ def send_rules(update, chat_id, from_pm=False):
             bot.send_message(
                 user.id,
                 "The rules shortcut for this chat hasn't been set properly! Ask admins to "
-                "fix this.\nMaybe they forgot the hyphen in ID",
-            )
+                "fix this.\nMaybe they forgot the hyphen in ID")
             return
         else:
             raise
@@ -46,32 +44,26 @@ def send_rules(update, chat_id, from_pm=False):
 
     if from_pm and rules:
         bot.send_message(
-            user.id, text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
-        )
+            user.id,
+            text,
+            parse_mode=ParseMode.MARKDOWN,
+            disable_web_page_preview=True)
     elif from_pm:
         bot.send_message(
             user.id,
             "The group admins haven't set any rules for this chat yet. "
-            "This probably doesn't mean it's lawless though...!",
-        )
+            "This probably doesn't mean it's lawless though...!")
     elif rules:
         update.effective_message.reply_text(
             "Please click the button below to see the rules.",
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(
-                            text="Rules", url=f"t.me/{bot.username}?start={chat_id}"
-                        )
-                    ]
-                ]
-            ),
-        )
+            reply_markup=InlineKeyboardMarkup([[
+                InlineKeyboardButton(
+                    text="Rules", url=f"t.me/{bot.username}?start={chat_id}")
+            ]]))
     else:
         update.effective_message.reply_text(
             "The group admins haven't set any rules for this chat yet. "
-            "This probably doesn't mean it's lawless though...!"
-        )
+            "This probably doesn't mean it's lawless though...!")
 
 
 @run_async
@@ -80,16 +72,18 @@ def set_rules(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     msg = update.effective_message  # type: Optional[Message]
     raw_text = msg.text
-    args = raw_text.split(None, 1)  # use python's maxsplit to separate cmd and args
+    args = raw_text.split(None,
+                          1)  # use python's maxsplit to separate cmd and args
     if len(args) == 2:
         txt = args[1]
-        offset = len(txt) - len(raw_text)  # set correct offset relative to command
+        offset = len(txt) - len(
+            raw_text)  # set correct offset relative to command
         markdown_rules = markdown_parser(
-            txt, entities=msg.parse_entities(), offset=offset
-        )
+            txt, entities=msg.parse_entities(), offset=offset)
 
         sql.set_rules(chat_id, markdown_rules)
-        update.effective_message.reply_text("Successfully set rules for this group.")
+        update.effective_message.reply_text(
+            "Successfully set rules for this group.")
 
 
 @run_async
@@ -106,7 +100,7 @@ def __stats__():
 
 def __import_data__(chat_id, data):
     # set chat rules
-    rules = data.get("info", {}).get("rules", "")
+    rules = data.get('info', {}).get('rules', "")
     sql.set_rules(chat_id, rules)
 
 
@@ -116,7 +110,6 @@ def __migrate__(old_chat_id, new_chat_id):
 
 def __chat_settings__(chat_id, user_id):
     return f"This chat has had it's rules set: `{bool(sql.get_rules(chat_id))}`"
-
 
 __help__ = """
  ❍ /rules*:* get the rules for this chat.
