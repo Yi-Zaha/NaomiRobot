24bdf6674fa05a4a315e034b43c0af39f18baf82
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-09-23 22:15:42 +0530

    Update antichannel.py

--- Naomi/modules/antichannel.py
+++ Naomi/modules/antichannel.py
@@ -30,7 +30,7 @@ def set_antichannel(update: Update, context: CallbackContext):
         "Antichannel setting is currently {} in {}".format(antichannel_status(chat.id), html.escape(chat.title)))
 
 
-@Naomimsg(Filters.chat_type.groups, group=110)
+@Naomimsg(Filters.ChatType.groups, group=110)
 def eliminate_channel(update: Update, context: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
