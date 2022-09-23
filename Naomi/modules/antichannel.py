ccf5dd0c403a73389cfe8c65b20f8385c86ec70c
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-09-23 18:57:24 +0530

    Update antichannel.py

--- Naomi/modules/antichannel.py
+++ Naomi/modules/antichannel.py
@@ -1,5 +1,5 @@
 from telegram.ext.filters import Filters
-from Shikimori.modules.helper_funcs.decorators import Naomicmd, Naomimsg
+from Naomi.modules.helper_funcs.decorators import Naomicmd, Naomimsg
 from telegram import Update, message
 from telegram.ext import CallbackContext
 from Naomi.modules.helper_funcs.anonymous import user_admin, AdminPerms
@@ -7,7 +7,7 @@ import html
 from Naomi.modules.sql.antichannel_sql import antichannel_status, disable_antichannel, enable_antichannel
 
 
-@Shikimoricmd(command="antichannel", group=100)
+@Naomicmd(command="antichannel", group=100)
 @user_admin(AdminPerms.CAN_RESTRICT_MEMBERS)
 def set_antichannel(update: Update, context: CallbackContext):
     message = update.effective_message
@@ -30,7 +30,7 @@ def set_antichannel(update: Update, context: CallbackContext):
         "Antichannel setting is currently {} in {}".format(antichannel_status(chat.id), html.escape(chat.title)))
 
 
-@Shikimorimsg(Filters.chat_type.groups, group=110)
+@Naomimsg(Filters.chat_type.groups, group=110)
 def eliminate_channel(update: Update, context: CallbackContext):
     message = update.effective_message
     chat = update.effective_chat
