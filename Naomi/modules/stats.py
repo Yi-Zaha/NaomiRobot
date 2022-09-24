3812e02eaccf3054aea658d10785981827e3518b
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-09-24 12:29:33 +0530

    Update stats.py

--- Naomi/modules/stats.py
+++ Naomi/modules/stats.py
@@ -14,7 +14,6 @@ from Naomi import (
     dispatcher,
     StartTime,
     SUPPORT_CHAT,
-    UPDATE_CHANNEL,
     START_IMG,)
 from Naomi.__main__ import STATS
 from Naomi.modules.helper_funcs.chat_status import sudo_plus
@@ -91,7 +90,7 @@ def stats(update, context):
                         "\n*Bot statistics*:\n"
                         + "\n".join(mod.__stats__() for mod in STATS)
                     )
-                    + f"\n\n✦ [Support](https://t.me/{SUPPORT_CHAT}) | ✦ [Updates](https://t.me/{UPDATE_CHANNEL})\n\n"
+                    + f"\n\n✦ [Support](https://t.me/{SUPPORT_CHAT}) | ✦ [Updates](https://t.me/{SUPPORT_CHAT})\n\n"
                 )
             ),
             parse_mode=ParseMode.MARKDOWN,
