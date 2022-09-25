2469320ed44aa0e3ab7ddf63141afa66370f4c99
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-09-25 18:29:38 +0530

    Update anonymous.py

--- Naomi/modules/helper_funcs/anonymous.py
+++ Naomi/modules/helper_funcs/anonymous.py
@@ -36,7 +36,7 @@ def user_admin(permission: AdminPerms):
             if update.effective_chat.type == 'private':
                 return func(update, context, *args, **kwargs)
             message = update.effective_message
-            is_anon = bool(update.effective_message.sender_chat)
+            is_anon = bool(update.effective_message.chat_id)
 
             if is_anon:
                 callback_id = f'anoncb/{message.chat.id}/{message.message_id}/{permission.value}'
