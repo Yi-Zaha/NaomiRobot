9aecf691cb2106b0dfe53fa908633d8a7bdc1306
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-11-25 22:42:02 +0530

    Update mongo.py

--- Naomi/mongo.py
+++ Naomi/mongo.py
@@ -5,7 +5,7 @@ from motor import motor_asyncio
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
 from Naomi.confing import get_int_key, get_str_key
-MONGO_DB_URI="mongodb+srv://raj1:raj1@cluster0.wtuav.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URI="mongodb+srv://raj:raj@cluster0.wtuav.mongodb.net/?retryWrites=true&w=majority"
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
@@ -19,3 +19,5 @@ db = motor[MONGO_DB]
 db = client["raj"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
+except ServerSelectionTimeoutError:
+    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
