6f3b63bc53c40c52ef1f3c3226a0b454820ca864
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-11-25 17:01:56 +0530

    Create con.py

---
+++ con.py
@@ -0,0 +1,23 @@
+
+ALLOW_EXCL="True"
+API_HASH="c0da9c346d2c45dbc7ec49a05da9b2b6"
+API_ID=13675555
+ARQ_API_KEY="LJMETG-DPHBCX-DGHJCD-TMFIGB-ARQ"
+ARQ_API_URL="https://arq.hamker.in"
+BOT_ID=5548310123
+CASH_API_KEY="LUCXC3WOGJG5Z0VI"
+DATABASE_URL="postgres://bovnygnt:HbpZhz6T6Koeub6B-QfpgcIyYD4HpeZU@jelani.db.elephantsql.com/bovnygnt"
+DRAGONS=5066050315
+ENV = "ANYTHING"
+EVENT_LOGS = -1001638398396
+JOIN_LOGGER = -1001638398396
+MONGO_DB_URI="mongodb+srv://raj1:raj1@cluster0.wtuav.mongodb.net/?retryWrites=true&w=majority"
+NO_LOAD = "antinsfw"
+OWNER_ID=5591954930
+OWNER_USERNAME="cant_think_1"
+SPAMWATCH_API="RW8OiWPawUgvbZY1oH5UH5PsH5_kPMd6RUy7EAOSZrNKB5rufVGHvR3hfBnUHKh_"
+START_IMG="https://telegra.ph/file/755a979e1e5bfb6fc5c0b.jpg"
+SUPPORT_CHAT = "naomi_supp"
+TIME_API_KEY = "VZCX9WHJTKED"
+Edit Delete
+TOKEN = "5548310123:AAFD0VhhA0CIkn9v2Z24grg2bvIp_-_Zrs0"
