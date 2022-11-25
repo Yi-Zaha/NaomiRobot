0b12e63a92f04fabd3d53fb5e98c955fe15204b8
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2022-11-25 17:19:41 +0530

    Update __init__.py

--- Naomi/__init__.py
+++ Naomi/__init__.py
@@ -105,7 +105,7 @@ if ENV:
         raise Exception("Your blacklisted chats list does not contain valid integers.")
 
 else:
-    from FallenRobot.config import Development as Config
+    from Naomi.config import Development as Config
 
     TOKEN = Config.TOKEN
 
