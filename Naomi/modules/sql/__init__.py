034b54f008170978296e59d304d35107c8e53a25
tyburd <102765502+tyburd@users.noreply.github.com>
2022-12-15 16:15:39 +0530

    Update __init__.py

--- Naomi/modules/sql/__init__.py
+++ Naomi/modules/sql/__init__.py
@@ -11,7 +11,6 @@ if DB_URI and DB_URI.startswith("postgres://"):
 
 def start() -> scoped_session:
     engine = create_engine(DB_URI, client_encoding="utf8")
-    log.info("[PostgreSQL] Connecting to database......")
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
@@ -21,5 +20,4 @@ BASE = declarative_base()
 try:
     SESSION = start()
 except Exception as e:
-    log.exception(f"[PostgreSQL] Failed to connect due to {e}")
     exit()
