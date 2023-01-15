1ca8a5776ae46670912c49a2096a20914ebdffa1
tyburd <102765502+tyburd@users.noreply.github.com>
2023-01-15 20:06:35 +0530

    Update telegraph.py

--- Naomi/modules/telegraph.py
+++ Naomi/modules/telegraph.py
@@ -8,7 +8,7 @@ from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 
-Anonymous = "Fallen"
+Anonymous = "kagut"
 telegraph = Telegraph()
 r = telegraph.create_account(short_name=Anonymous)
 auth_url = r["auth_url"]
@@ -50,7 +50,7 @@ async def _(event):
                 )
         elif input_str == "t":
             user_object = await tbot.get_entity(r_message.sender_id)
-            title_of_page = user_object.first_name  # + " " + user_object.last_name
+            title_of_page = user_object.first_name  # + " " + user_object.last_name + "By @Naomi_Robot"
             # apparently, all Users do not have last_name field
             if optional_title:
                 title_of_page = optional_title
