ea904686d477958460adadfba15e3f4dac37c27c
tyburd <102765502+tyburd@users.noreply.github.com>
2023-06-28 17:29:01 +0530

    Update chatbot.py

--- Naomi/modules/chatbot.py
+++ Naomi/modules/chatbot.py
@@ -131,7 +131,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get(f'https://www.kukiapi.xyz/api/apikey=KUKIg76Fg4EIo/Cutiepii/@Awesome_RJ/message='+Message)
+        kukiurl = requests.get(f'https://mizo.otakatsu.studio/mizo/chatbot?message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
         sleep(0.3)
