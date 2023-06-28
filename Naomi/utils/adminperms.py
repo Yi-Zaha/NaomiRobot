abd0e365942b61faec306636df97dea6a0014bcf
tyburd <102765502+tyburd@users.noreply.github.com>
2023-06-28 17:39:51 +0530

    Update adminperms.py

--- Naomi/utils/adminperms.py
+++ Naomi/utils/adminperms.py
@@ -7,22 +7,22 @@ from Naomi import pbot as app
 async def member_permissions(chat_id: int, user_id: int):
     perms = []
     member = await app.get_chat_member(chat_id, user_id)
-    if member.can_send_messages:
-        perms.append("can_send_messages")
-    if member.can_edit_messages:
+    if member.privileges.can_post_messages:
+        perms.append("can_post_messages")
+    if member.privileges.can_edit_messages:
         perms.append("can_edit_messages")
-    if member.can_delete_messages:
+    if member.privileges.can_delete_messages:
         perms.append("can_delete_messages")
-    if member.can_restrict_members:
+    if member.privileges.can_restrict_members:
         perms.append("can_restrict_members")
-    if member.can_promote_members:
+    if member.privileges.can_promote_members:
         perms.append("can_promote_members")
-    if member.can_change_info:
+    if member.privileges.can_change_info:
         perms.append("can_change_info")
-    if member.can_invite_users:
+    if member.privileges.can_invite_users:
         perms.append("can_invite_users")
-    if member.can_pin_messages:
+    if member.privileges.can_pin_messages:
         perms.append("can_pin_messages")
-    if member.can_manage_voice_chats:
+    if member.privileges.can_manage_voice_chats:
         perms.append("can_manage_voice_chats")
     return perms
