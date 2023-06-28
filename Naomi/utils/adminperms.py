1e70d112f58a0bbd6a8b0deb554c2bef7f99ac8c
tyburd <102765502+tyburd@users.noreply.github.com>
2023-06-28 16:55:56 +0530

    Update adminperms.py

--- Naomi/utils/adminperms.py
+++ Naomi/utils/adminperms.py
@@ -8,7 +8,7 @@ async def member_permissions(chat_id: int, user_id: int):
     perms = []
     member = await app.get_chat_member(chat_id, user_id)
     if member.can_send_messages:
-        perms.append("can_post_messages")
+        perms.append("can_send_messages")
     if member.can_edit_messages:
         perms.append("can_edit_messages")
     if member.can_delete_messages:
