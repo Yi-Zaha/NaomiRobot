0e13202fc092e232ec6ef8504b049850cb352e2f
tyburd <102765502+tyburd@users.noreply.github.com>
2023-06-29 23:51:18 +0530

    Update antinsfw.py

--- Naomi/modules/antinsfw.py
+++ Naomi/modules/antinsfw.py
@@ -141,7 +141,7 @@ async def nsfw_scan_command(_, message):
 
 
 @pbot.on_message(filters.command("antinsfw") & ~filters.private)
-@adminsOnly
+@adminsOnly("can_change_info")
 async def nsfw_enable_disable(_, message):
     if len(message.command) != 2:
         await message.reply_text(
