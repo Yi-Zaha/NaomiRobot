d2ece10b276575a838cc8dc856dbf06dfddb6d58
tyburd <102765502+tyburd@users.noreply.github.com>
2023-06-29 23:19:22 +0530

    Update eval.py

--- Naomi/modules/eval.py
+++ Naomi/modules/eval.py
@@ -49,8 +49,59 @@ def send(msg, bot, update):
 @dev_plus
 @run_async
 def evaluate(update: Update, context: CallbackContext):
-    bot = context.bot
-    send(do(eval, bot, update), bot, update)
+  try:
+        cmd = event.text.markdown.split(" ", maxsplit=1)[1]
+    except IndexError:
+        return await event.reply("`Give something to execute...`")
+    e = await event.reply("`Processing ...`")
+    reply_to_id = event.reply_to_message_id or event.id
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+
+    final_output = "**✦ Code :**\n`{}` \n\n **✦ Result :** \n`{}` \n".format(
+        cmd, evaluation
+    )
+    if len(final_output) > 4096:
+        with io.BytesIO(str.encode(evaluation)) as out_file:
+            out_file.name = "eval.txt"
+            await app.send_document(
+                event.chat.id,
+                out_file,
+                force_document=True,
+                caption=f"`{cmd}`" if len(cmd) < 998 else None,
+                parse_mode=ParseMode.MARKDOWN,
+                reply_to_message_id=reply_to_id,
+            )
+            await e.delete()
+    else:
+        await e.edit(
+            final_output, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
+        )
+
 
 
 @dev_plus
