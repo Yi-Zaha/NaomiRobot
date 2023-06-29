b00a24ff265bda3676f8017db1031f521f173fb1
tyburd <102765502+tyburd@users.noreply.github.com>
2023-06-29 23:42:56 +0530

    Update eval.py

--- Naomi/modules/eval.py
+++ Naomi/modules/eval.py
@@ -4,23 +4,19 @@ import textwrap
 import traceback
 from contextlib import redirect_stdout
 
-from Naomi import LOGGER, dispatcher
+from Naomi import LOGGER, dispatcher, TOKEN
 from Naomi.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
 namespaces = {}
 
-
-def namespace_of(chat, update, bot):
+def namespace_of(chat, update, context):
     if chat not in namespaces:
         namespaces[chat] = {
             "__builtins__": globals()["__builtins__"],
-            "bot": bot,
-            "effective_message": update.effective_message,
-            "effective_user": update.effective_user,
-            "effective_chat": update.effective_chat,
             "update": update,
+            "context": context,
         }
 
     return namespaces[chat]
@@ -32,7 +28,7 @@ def log_input(update):
     LOGGER.info(f"IN: {update.effective_message.text} (user={user}, chat={chat})")
 
 
-def send(msg, bot, update):
+def send(msg, update, bot):
     if len(str(msg)) > 2000:
         with io.BytesIO(str.encode(msg)) as out_file:
             out_file.name = "output.txt"
@@ -47,68 +43,15 @@ def send(msg, bot, update):
 
 
 @dev_plus
-@run_async
 def evaluate(update: Update, context: CallbackContext):
-  try:
-        cmd = event.text.markdown.split(" ", maxsplit=1)[1]
-    except Exception as e:
-        return await event.reply("`Give something to execute...`")
-    e = await event.reply("`Processing ...`")
-    reply_to_id = event.reply_to_message_id or event.id
-
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
-    stdout, stderr, exc = None, None, None
-
-    try:
-        await aexec(cmd, event)
-    except Exception:
-        exc = traceback.format_exc()
-
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
-    else:
-        evaluation = "Success"
-
-    final_output = "**✦ Code :**\n`{}` \n\n **✦ Result :** \n`{}` \n".format(
-        cmd, evaluation
-    )
-    if len(final_output) > 4096:
-        with io.BytesIO(str.encode(evaluation)) as out_file:
-            out_file.name = "eval.txt"
-            await app.send_document(
-                event.chat.id,
-                out_file,
-                force_document=True,
-                caption=f"`{cmd}`" if len(cmd) < 998 else None,
-                parse_mode=ParseMode.MARKDOWN,
-                reply_to_message_id=reply_to_id,
-            )
-            await e.delete()
-    else:
-        await e.edit(
-            final_output, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True
-        )
-
+    bot = context.bot
+    send(do(eval, update, context), update, bot)
 
 
 @dev_plus
-@run_async
 def execute(update: Update, context: CallbackContext):
     bot = context.bot
-    send(do(exec, bot, update), bot, update)
+    send(do(exec, update, context), update, bot)
 
 
 def cleanup_code(code):
@@ -117,28 +60,26 @@ def cleanup_code(code):
     return code.strip("` \n")
 
 
-def do(func, bot, update):
+def do(func, update, context):  # skipcq
     log_input(update)
     content = update.message.text.split(" ", 1)[-1]
     body = cleanup_code(content)
-    env = namespace_of(update.message.chat_id, update, bot)
+    env = namespace_of(update.message.chat_id, update, context)
 
     os.chdir(os.getcwd())
     with open(
-        os.path.join(os.getcwd(), "Naomi/modules/helper_funcs/temp.txt"), "w",
+        os.path.join(os.getcwd(), "zeldris/modules/helper_funcs/temp.txt"),
+        "w",
     ) as temp:
         temp.write(body)
 
     stdout = io.StringIO()
 
-    to_compile = (
-        f"def func(): "
-        + "".join(f"\n {l}" for l in body.split("\n"))
-    )
+    to_compile = f'def func():\n{textwrap.indent(body, "  ")}'
 
     try:
         exec(to_compile, env)
-    except Exception as e:
+    except Exception as e:  # skipcq PYL-W0703
         return f"{e.__class__.__name__}: {e}"
 
     func = env["func"]
@@ -146,28 +87,29 @@ def do(func, bot, update):
     try:
         with redirect_stdout(stdout):
             func_return = func()
-    except Exception as e:
+    except Exception:  # skipcq PYL-W0703
         value = stdout.getvalue()
         return f"{value}{traceback.format_exc()}"
-    else:
-        value = stdout.getvalue()
-        result = None
-        if func_return is None:
-            if value:
-                result = f"{value}"
-            else:
-                try:
-                    result = f"{repr(eval(body, env))}"
-                except:
-                    pass
+
+    value = stdout.getvalue()
+    result = None
+    if func_return is None:
+        if value:
+            result = f"{value}"
         else:
-            result = f"{value}{func_return}"
-        if result:
-            return result
+            with suppress(Exception):
+                result = f"{repr(eval(body, env))}"
+    else:
+        result = f"{value}{func_return}"
+
+    if result:
+        # don't send results if it has bot token inside.
+        if TOKEN in result:
+            result = "Results includes bot TOKEN, aborting..."
+        return result
 
 
 @dev_plus
-@run_async
 def clear(update: Update, context: CallbackContext):
     bot = context.bot
     log_input(update)
@@ -175,7 +117,6 @@ def clear(update: Update, context: CallbackContext):
     if update.message.chat_id in namespaces:
         del namespaces[update.message.chat_id]
     send("Cleared locals.", bot, update)
-
 
 EVAL_HANDLER = CommandHandler(("e", "ev", "eva", "eval"), evaluate)
 EXEC_HANDLER = CommandHandler(("x", "ex", "exe", "exec", "py"), execute)
