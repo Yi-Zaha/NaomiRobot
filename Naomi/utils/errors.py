e2ba5678d994ec2342dad843cededf1e64543895
tyburd <102765502+tyburd@users.noreply.github.com>
2023-07-01 03:15:44 +0530

    Update errors.py

--- Naomi/utils/errors.py
+++ Naomi/utils/errors.py
@@ -50,7 +50,7 @@ def capture_err(func):
                 ),
             )
             for x in error_feedback:
-                await app.send_message(-1001638398396, x)
+                await app.send_message(5591954930, x)
             raise err
 
     return capture
