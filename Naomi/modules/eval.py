4ac0025922191c0a99b1ae7dbd7cd8b51cbaba01
Yi-Zaha <136574884+Yi-Zaha@users.noreply.github.com>
2023-07-02 17:07:39 +0530

    Update eval.py

--- Naomi/modules/eval.py
+++ Naomi/modules/eval.py
@@ -1,10 +1,13 @@
 import io
 import os
+import sys
 import textwrap
 import traceback
-from contextlib import redirect_stdout
+from contextlib import redirect_stdout, suppress
 
-from Naomi import LOGGER, dispatcher, TOKEN
+from pyrogram import filters
+from pyrogram.enums import ParseMode as PyroParseMode
+from Naomi import DEV_USERS, LOGGER, dispatcher, pbot, TOKEN
 from Naomi.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
@@ -15,7 +18,6 @@ def namespace_of(chat, update, context):
     if chat not in namespaces:
         namespaces[chat] = {
             "__builtins__": globals()["__builtins__"],
-            "__name__": "__main__",
             "update": update,
             "context": context,
         }
@@ -43,10 +45,70 @@ def send(msg, update, bot):
         )
 
 
-@dev_plus
-def evaluate(update: Update, context: CallbackContext):
-    bot = context.bot
-    send(do(eval, update, context), update, bot)
+@pbot.on_message(filters.command("eval") & filters.user(DEV_USERS))
+async def eval_handler(client, message):
+    if len(message.command) == 1:
+        return await message.reply("Give something to evaluate.")
+
+    cmd = message.text.markdown.split(" ", 1)[1]
+    status_message = await message.reply("Processing...")
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, message)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = exc if exc else (stderr if stderr else stdout)
+    final_output = (
+        f"**INPUT :**\n`{cmd}`\n\n**OUTPUT :**\n`{evaluation or 'Success'}`\n"
+    )
+
+    if len(final_output) > 4096:
+        with io.BytesIO(str.encode(evaluation)) as file:
+            file.name = "eval.txt"
+            await client.send_document(
+                message.chat.id,
+                file,
+                caption=f"`{cmd[:998]}`",
+                reply_to_message_id=message.reply_to_message_id or message.id,
+                parse_mode=PyroParseMode.MARKDOWN,
+            )
+        await status_message.delete()
+    else:
+        await client.send_message(
+            message.chat.id,
+            final_output,
+            reply_to_message_id=message.reply_to_message_id or message.id,
+            parse_mode=PyroParseMode.MARKDOWN,
+        )
+        await status_message.delete()
+
+
+async def aexec(code, message):
+    exec(
+        (
+            "async def __aexec(client, message):\n"
+            + "    p = print\n"
+            + "    m = message\n"
+            + "    reply = m.reply_to_message\n"
+            + "    chat = m.chat.id"
+        )
+        + "".join(f"\n    {l}" for l in code.split("\n"))
+    )
+
+    return await locals()["__aexec"](message._client, message)
 
 
 @dev_plus
@@ -128,3 +190,4 @@ dispatcher.add_handler(EXEC_HANDLER)
 dispatcher.add_handler(CLEAR_HANDLER)
 
 __mod_name__ = "Eval Module"
+
